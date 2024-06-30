047cd5d31ef8fad1b0aefa9aebda8d16b063271e
theaholstensson <thea.holstensson@hotmail.com>
2024-06-30 18:13:49 +0200

    Get Courses

--- Infrastructure/Contexts/ApiContext.cs
+++ Infrastructure/Contexts/ApiContext.cs
@@ -6,4 +6,5 @@ namespace Infrastructure.Contexts;
 public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
 {
     public DbSet<SubscribersEntity> Subscribers { get; set; }
+    public DbSet<CourseEntity> Courses { get; set; }
 }
--- WebApi/Controllers/CoursesController.cs
+++ WebApi/Controllers/CoursesController.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CoursesController : ControllerBase
+public class CoursesController(ApiContext context) : ControllerBase
 {
+    private readonly ApiContext _context = context;
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        return Ok();
+        var courses = await _context.Courses.ToListAsync();
+        return Ok(courses);
     }
 }
